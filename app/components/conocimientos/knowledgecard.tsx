31018335fb727c942ea0a8d943d5795e6472a0c8
Cristian Julio <cristian.juliom@udea.edu.co>
2024-04-06 14:24:30 -0500

    Agregando sesión de conocmientos al contenido central

--- app/components/conocimientos/knowledgecard.tsx
+++ app/components/conocimientos/knowledgecard.tsx
@@ -8,10 +8,10 @@ interface Props {
 
 const KnowledgeCard: React.FC<Props> = ({ title, description, imageSrc }) => {
   return (
-    <div className="w-[310px] h-[225px]  bg-white flex flex-col items-center justify-center">
-      <img src={imageSrc} alt={`${title} image`} width={68} height={68} className="top-[720.09px] left-[130px] mb-6" /> {/* Usa img para mostrar la imagen */}
-      <h3 className="w-[158px] h-[22px] top-[814.09px] left-[85px] text-left font-inter font-medium text-lg leading-[22.25px] mb-6">{title}</h3>
-      <p className="w-[134px] h-[24px] top-[851.09px] left-[97px] text-center font-inter text-base leading-6">{description}</p>
+    <div className="w-[310px] h-[225px]  bg-white flex flex-col items-center justify-center whitespace-nowrap">
+      <img src={imageSrc} alt={`${title} image`} width={68} height={68} className="top-[720.09px] left-[130px] mb-2" /> {/* Usa img para mostrar la imagen */}
+      <h3 className="font-bold text-lg mb-2">{title}</h3>
+      <p className=" text-sm ">{description}</p>
     </div>
   );
 };
--- app/components/conocimientos/maincomponent2.tsx
+++ app/components/conocimientos/maincomponent2.tsx
@@ -3,15 +3,13 @@ import KnowledgeCard from './knowledgecard';
 
 const MainComponent2 = () => {
   return (
-    <div className="w-[970px] h-[653px] flex flex-wrap justify-around">
+    <div className="w-[970px] h-[653px] flex flex-wrap justify-around p-12">
       <h2 className="w-full text-center font-inter font-bold text-2xl leading-[39.55px] mb-4">My Knowledge</h2>
-      <p className="w-full text-center font-inter text-base leading-6 mb-8">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit. lorem ipsum</p>
-      <KnowledgeCard title="Web Development" description="blog, e-commerce" imageSrc="/facebook.png" /> {/* Reemplaza "/ruta/a/tu/imagen.jpg" con la ruta a tu imagen */}
-      <KnowledgeCard title="Título del conocimiento" description="Descripción del conocimiento" imageSrc="/facebook.png" />
-      <KnowledgeCard title="Título del conocimiento" description="Descripción del conocimiento" imageSrc="/facebook.png" />
-      <KnowledgeCard title="Título del conocimiento" description="Descripción del conocimiento" imageSrc="/facebook.png" />
-      <KnowledgeCard title="Título del conocimiento" description="Descripción del conocimiento" imageSrc="/facebook.png" />
-      <KnowledgeCard title="Título del conocimiento" description="Descripción del conocimiento" imageSrc="/facebook.png" />
+      <p className="w-full text-center font-inter text-base leading-6 mb-8">In this session I will address the relevant topics with some knowledge acquired in my academic and professional life.</p>
+      <KnowledgeCard title="Python Development" description="Machine learning, Deep learning" imageSrc="/python.png" /> {/* Reemplaza "/ruta/a/tu/imagen.jpg" con la ruta a tu imagen */}
+      <KnowledgeCard title="Security Analyst" description="Devsecops, SAST, DAST" imageSrc="/secure.png" />
+      <KnowledgeCard title="Web Development" description="Html, CSS, Js, Tailwind" imageSrc="/web.png" />  
+      <KnowledgeCard title=".Net Development" description=".Net Api, JWT, SQL-Server" imageSrc="/ch.png" />     
       
       {/* Más tarjetas de conocimientos */}
     </div>
